2f5a847a9086c098415e39725fec236b64fedf2a
Khair Al Anam <84736189+khairalanam@users.noreply.github.com>
2021-11-04 20:04:57 +0300

    New code block
    Added new code for Budget Calculator component and added new function for user input

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
 from classes import Expenditure
-# Main Program
 
-# Expenditure Allocator Component
-yes_or_no = "y"
+# Functions used
 
-while yes_or_no != "n":
-    income = int(input("Enter your monthly income(in dollars): "))
-    budget = int(input("Enter your total budget(in dollars): "))
-    tax = int(input("Enter the monthly tax rate(in %): "))
-    expense = Expenditure(income, budget, tax)
-    print(expense.allocate())
-    print("*****************************")
+
+def y_or_n():
+    global yes_or_no
     yes_or_no = input("Continue? (y/n): ")
     if yes_or_no in ["y", "n"]:
-        pass
+        return yes_or_no
     else:
         print("Invalid input!")
-        continue
+        y_or_n()
+
+
+# Main Program
+print("Welcome to the Money App!")
+print("1. Expenditure Allocator")
+print("2. Budget Calculator")
+print("3. (Coming Soon)")
+
+selector = int(input(
+    "Type the number to proceed to any one of the three components mentioned above: "))
+
+yes_or_no = "y"
+
+# Expenditure Allocator Component
+
+if selector == 1:
+    while yes_or_no != "n":
+        income = int(input("Enter your monthly income(in dollars): "))
+        budget = int(input("Enter your monthly budget(in dollars): "))
+        tax = int(input("Enter the monthly tax rate(in %): "))
+        expense = Expenditure(income, budget, tax)
+        print(expense.allocate())
+        print("*****************************")
+        yes_or_no = y_or_n()
+
+# Budget Calculator Component
+
+if selector == 2:
+    while yes_or_no != "n":
+        income = int(input("Enter your monthly income(in dollars): "))
+        budget = int(input("Enter your monthly budget(in dollars): "))
+        tax = int(input("Enter the monthly tax rate(in %): "))
+        expense = Expenditure(income, budget, tax)
+
+        print("1. Monthly Budget Calculator")
+        print("2. Time Calculator")
+        print("3. Budget per Month Calculator")
+        budget_selector = int(input(
+            "Enter the number to proceed to any one of the above mentioned sub-components: "))
+
+        yes_or_no = y_or_n()
