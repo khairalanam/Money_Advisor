07458a933faad7811e838f3ee66ddb1c1311152e
Khair Al Anam <84736189+khairalanam@users.noreply.github.com>
2021-11-29 21:05:11 +0530

    Final main.py

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ def y_or_n():
 
 
 # Main Program
+
 print("Welcome to the Money Advisor!")
-print("1. Expenditure Allocator")
+print("1. Expenditure Calculator")
 print("2. Budget Calculator")
 
 selector = 0
@@ -30,19 +31,35 @@ while selector not in [1, 2]:
 
 yes_or_no = "y"
 
-# Expenditure Allocator Component
+# Expenditure Calculator Component
 
 if selector == 1:
     while yes_or_no != "n":
         income = int(input("Enter your monthly income(in dollars): "))
         budget = int(input("Enter your monthly budget(in dollars): "))
         tax = int(input("Enter the monthly tax rate(in %): "))
-        expense = Expenditure(income, budget, tax)
-        print("*****************************")
-        print("*****************************")
-        print(expense.allocate())
-        print("*****************************")
-        print("*****************************")
+        Expense = Expenditure(income, budget, tax)
+
+        print("1. Monthly Expenditure Calculator")
+        print("2. Tax Calculator")
+        print("3. Expenditure Allocator")
+        expense_selector = int(input(
+            "Enter the number to proceed to any one of the above mentioned sub-components: "))
+
+        if expense_selector == 1:
+            print(
+                f"Your monthly expenditure is {Expense.expenses()} dollars per month.")
+
+        if expense_selector == 2:
+            print(
+                f"Your monthly tax is {Expense.tax_amount()} dollars per month.")
+
+        if expense_selector == 3:
+            print("*****************************")
+            print("*****************************")
+            print(Expense.allocate())
+            print("*****************************")
+            print("*****************************")
 
         yes_or_no = y_or_n()
 
@@ -63,12 +80,12 @@ if selector == 2:
 
         if budget_selector == 1:
             print(
-                f"Your monthly budget is {expense.budget()} dollars per month")
+                f"Your monthly budget is {expense.budget()} dollars per month with all expenses calculated.")
 
         if budget_selector == 2:
             total = int(input("Type in the total budget to be fulfilled: "))
             print(
-                f"You will need {expense.months_needed(total)} months to  fulfill your full budget, given that your monthly budget is {expense.budget()} dollars.")
+                f"You will need {expense.months_needed(total)} months to  fulfill your full budget, given that your monthly budget is {expense.monthly_budget} dollars.")
 
         if budget_selector == 3:
             full_budget = int(
@@ -76,6 +93,6 @@ if selector == 2:
             time = int(
                 input("Type in the number of months required to fulfill the total budget: "))
             print(
-                f"You will need to have a monthly budget of f{Expenditure.budget_per_month(full_budget, time)} dollars per month in order to fulfill your total budget of {full_budget} within {time} months.")
+                f"You will need to have a monthly budget of {Expenditure.budget_per_month(full_budget, time)} dollars per month in order to fulfill your total budget of {full_budget} within {time} months.")
 
         yes_or_no = y_or_n()
